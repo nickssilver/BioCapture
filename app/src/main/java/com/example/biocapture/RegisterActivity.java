ac2a3f957d4c8ff4e2a8f2b430941a7143ed7342
nickssilver <silvernicks9@gmail.com>
2023-10-17 08:53:29 +0300

    Logic Register

--- app/src/main/java/com/example/biocapture/RegisterActivity.java
+++ app/src/main/java/com/example/biocapture/RegisterActivity.java
@@ -1,7 +1,8 @@
 package com.example.biocapture;
-
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -10,5 +11,24 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        // Initialize UI elements
+        studentNumberEditText = findViewById(R.id.editTextStudentNo);
+        studentNameEditText = findViewById(R.id.editTextStudentName);
+        courseEditText = findViewById(R.id.editTextCourse);
+        departmentEditText = findViewById(R.id.editTextDepartment);
+        statusEditText = findViewById(R.id.editTextStatus);
+        classEditText = findViewById(R.id.editTextClass);
+
+        submitButton = findViewById(R.id.buttonSubmitReg);
+
+        // Set up event listeners for the submit button or any other elements as needed
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Implement your registration logic here
+                // You can access the user input using the UI elements (e.g., studentNumberEditText.getText().toString())
+                // and send the data to your server.
+            }
+        });
     }
 }
