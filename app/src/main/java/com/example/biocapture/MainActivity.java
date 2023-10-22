008faeeeff57aed0513c1d09cdc3e63ad2208ef8
nickssilver <silvernicks9@gmail.com>
2023-10-23 01:49:38 +0300

    Added Menu

--- app/src/main/java/com/example/biocapture/MainActivity.java
+++ app/src/main/java/com/example/biocapture/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.biocapture;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.verify)
+        {
+            Intent intent =new Intent(this, VerifyActivity.class);
+            startActivities(new Intent[]{intent});
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
