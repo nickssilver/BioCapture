ceadc613eda15c2ef575d23ad6a0bc836a665fb6
nicks <silvernicks9@gmail.com>
2024-03-12 16:39:10 +0300

    Tversion un-added files

---
+++ MorphoLiveScan/build/intermediates/cxx/Debug/6v2j1g4m/logs/armeabi-v7a/build_command_mls-jni.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\Software Engineer\\AppData\\Local\\Android\\Sdk\\cmake\\3.10.2.4988404\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\Software Engineer\\AndroidStudioProjects\\BioCapture\\MorphoLiveScan\\.cxx\\Debug\\6v2j1g4m\\armeabi-v7a" ^
+  mls-jni
